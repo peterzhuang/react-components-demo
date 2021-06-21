edb8b235f332b37289b6d6e04404132f49c4860b
yuchuan zhuang <yuchuan.zhuang@mindgeek.com>
2021-06-21 18:12:02 -0400

    drag and drop demo

---
+++ src/AppDragDropDemo.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class AppDragDropDemo extends Component {
+    state = {
+        tasks: [
+            {name:"Learn Angular",category:"wip", bgcolor: "yellow"},
+            {name:"React", category:"wip", bgcolor:"pink"},
+            {name:"Vue", category:"complete", bgcolor:"skyblue"}
+        ]
+    }
+
+    onDragStart = (e, id) => {
+        console.log('dragstart:', id);
+        e.dataTransfer.setData("text/plain", id);
+    }
+
+    onDragOver = e => {
+        e.preventDefault();
+
+    }
+
+    onDrop = (e, cat) => {
+        let id = e.dataTransfer.getData("text/plain");
+
+        let tasks = this.state.tasks.filter(task => {
+            if (task.name === id) {
+                task.category = cat;
+            }
+            return task;
+        });
+
+        this.setState({
+            ...this.state,
+            tasks,
+        })
+
+    }
+
+    render() {
+        var tasks = {
+            wip: [],
+            complete: []
+        }
+
+        this.state.tasks.forEach(t => {
+            tasks[t.category].push(
+                <div 
+                    onDragStart={e => this.onDragStart(e, t.name)}
+                    draggable
+                    key={t.name} 
+                    className="draggable" 
+                    style={{backgroundColor: t.bgcolor}}
+                >   
+                    {t.name}
+                </div>
+            );
+        });
+
+
+        return (
+            <div className="container-drag">
+               <h2 className="header">DRAG & DROP DEMO</h2>
+               <div 
+                    className="wip"
+                    onDragOver={e => this.onDragOver(e)}
+                    onDrop={e => this.onDrop(e, "wip")}
+               >
+                    <span className="task-header">WIP</span>
+                    {tasks.wip}
+               </div>
+               <div 
+                    className="droppable" 
+                    onDragOver={e => this.onDragOver(e)}
+                    onDrop={e => this.onDrop(e, "complete")}
+               >
+                    <span className="task-header">COMPLETED</span>
+                    {tasks.complete}
+               </div>
+            </div>
+        );
+    }
+}
